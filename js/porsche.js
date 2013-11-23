972fe8ed1cc485a53dfc097e4f84e8c48bde1311
Stephanie Albert <salbert61@gmail.com>
2013-11-23 14:37:33 -0500

    Massive mistake
    Trying to apply masonry.js

--- js/porsche.js
+++ js/porsche.js
@@ -2,14 +2,21 @@
 ---
 
 {% include js/jquery.nivo.slider.pack.js %}
-
-
 $(window).load(function() {
     $('#slider').nivoSlider();
 });
 
-{% include js/modernizr.custom.04457.js %}
 
+{% include js/modernizr.custom.04457.js %}
 $(window).load(function() {
     $('#modernizr').modernizer ();
+});
+
+
+{% include js/masonry.pkgd.min.js %}
+var container = document.querySelector('#container');
+var msnry = new Masonry( container, {
+  // options
+  columnWidth: 200,
+  itemSelector: '.unit'
 });
