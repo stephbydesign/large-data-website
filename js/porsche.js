664825cd663daa17f16153815587fecb878f205e
Stephanie Albert <salbert61@gmail.com>
2013-11-24 11:29:16 -0500

    Revert e96a4eb..ab318d5
    This rolls back to commit e96a4eb795a47d99a0403aa566cf871a5452314f.

--- js/porsche.js
+++ js/porsche.js
@@ -2,25 +2,9 @@
 ---
 
 {% include js/jquery.nivo.slider.pack.js %}
+
+
 $(window).load(function() {
     $('#slider').nivoSlider();
 });
 
-<<<<<<< HEAD
-
-{% include js/modernizr.custom.04457.js %}
-$(window).load(function() {
-    $('#modernizr').modernizer ();
-});
-
-
-{% include js/masonry.pkgd.min.js %}
-var container = document.querySelector('#container');
-var msnry = new Masonry( container, {
-  // options
-  columnWidth: 200,
-  itemSelector: '.unit'
-});
-
-=======
->>>>>>> parent of b1bf067... Moved modernizr to includes.
